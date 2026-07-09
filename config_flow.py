b5fbb15bec3c90cbfb1fa9c7e1b1d33fb8b54667
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-09 17:42:39 -0600

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers import selector
 
 from .const import (
@@ -17,7 +18,7 @@ class AlexaClimateBridgeConfigFlow(
     config_entries.ConfigFlow,
     domain=DOMAIN,
 ):
-    """Alexa Climate Bridge Config Flow."""
+    """Config Flow."""
 
     VERSION = 1
 
@@ -29,17 +30,54 @@ class AlexaClimateBridgeConfigFlow(
                 data=user_input,
             )
 
+        device_registry = dr.async_get(self.hass)
+
+        echo_options = []
+
+        for device in device_registry.devices.values():
+
+            identifiers = list(device.identifiers)
+
+            if not identifiers:
+                continue
+
+            found = False
+
+            for identifier in identifiers:
+                if identifier[0] == "alexa_devices":
+                    found = True
+                    break
+
+            if not found:
+                continue
+
+            echo_options.append(
+                selector.SelectOptionDict(
+                    value=device.id,
+                    label=device.name,
+                )
+            )
+
+        echo_options.sort(key=lambda item: item["label"])
+
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_NAME): selector.TextSelector(),
 
-                    vol.Required(CONF_ECHO_DEVICE): selector.DeviceSelector(
-                        selector.DeviceSelectorConfig()
+                    vol.Required(
+                        CONF_ECHO_DEVICE,
+                    ): selector.SelectSelector(
+                        selector.SelectSelectorConfig(
+                            options=echo_options,
+                            mode=selector.SelectSelectorMode.DROPDOWN,
+                        )
                     ),
 
-                    vol.Required(CONF_CLIMATE_NAME): selector.TextSelector(),
+                    vol.Required(
+                        CONF_CLIMATE_NAME,
+                    ): selector.TextSelector(),
                 }
             ),
         )
