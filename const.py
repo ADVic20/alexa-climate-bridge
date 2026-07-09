99950f12a9003088badcdc7928d15f68064fb18c
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-09 08:19:07 -0600

    Actualizar const.py

--- const.py
+++ const.py
@@ -1,5 +1,15 @@
-DOMAIN = "alexa-climate-bridge"
+DOMAIN = "alexa_climate_bridge"
+
+NAME = "Alexa Climate Bridge"
 
 CONF_NAME = "name"
 CONF_ECHO_DEVICE = "echo_device"
 CONF_CLIMATE_NAME = "climate_name"
+
+MANUFACTURER = "Alexa Climate Bridge"
+MODEL = "Virtual Climate"
+
+DEFAULT_MIN_TEMP = 16
+DEFAULT_MAX_TEMP = 30
+
+VERSION = "0.1.0"
