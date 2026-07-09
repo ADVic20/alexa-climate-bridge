9de2dec3cde8dd817b53213d52e5043c0989e112
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-09 17:48:06 -0600

    Add new constants for Echo integration

--- const.py
+++ const.py
@@ -6,6 +6,11 @@ CONF_NAME = "name"
 CONF_ECHO_DEVICE = "echo_device"
 CONF_CLIMATE_NAME = "climate_name"
 
+CONF_ECHO_EVENT = "echo_event"
+CONF_ECHO_MEDIA_PLAYER = "echo_media_player"
+CONF_ECHO_NAME = "echo_name"
+CONF_ECHO_DEVICE_ID = "echo_device_id"
+
 MANUFACTURER = "Alexa Climate Bridge"
 MODEL = "Virtual Climate"
 
